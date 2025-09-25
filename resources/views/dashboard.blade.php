a5c0507ba0949d430a079329be79f87c2d3d3cea
ashish aryal <ashishdada@gmail.com>
2025-09-25 16:18:24 +0200

    basic layout setup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,18 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 my-4">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h2 class="text-2xl">Road Trips you own</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h2 class="text-2xl">Road Trips you are invited to</h2>
                 </div>
             </div>
         </div>
