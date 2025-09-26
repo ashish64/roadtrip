7006b0cbadea260f37ecca45098570314b9189c7
ashish aryal <ashishdada@gmail.com>
2025-09-26 12:37:19 +0200

    relationships between suggestion, user and trip setup

--- app/Models/Suggestion.php
+++ app/Models/Suggestion.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Suggestion extends Model
 {
     //
+
+    protected $fillable = ['description', 'status'];
+
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function Trip(): BelongsTo
+    {
+        return $this->belongsTo(Trip::class);
+    }
 }
